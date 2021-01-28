5035803189fd84c33081db988b95cbf729328e73
MityaKhlopyanikov <dzmitry.khlapianikau@innowise-group.com>
2021-01-28 11:32:16 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div className="App" style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <div style={{ fontSize: '60px' }}>
+        ОЛЯ, НЕ ГРУСТИ!!!
+      </div>
+    </div>
+  );
+}
+
+export default App;
